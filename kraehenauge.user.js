c8a9d956bf8d304d7c5946e5bae83e0bd16479c7
Johannes Dewender <jonerian@JonnyJD.net>
2008-12-12 04:09:49 +0000

    auge: version 1.2.1 (rc)
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@98 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -4,10 +4,10 @@
 // @description    Dies ist bzw. wird das clientseitige KSK-Programm. Es unterstuetzt die Kraehen auf ihren Wegen in Alirion und gibt ihnen Ueberblick und schnelle Reaktionsmoeglichkeiten.
 // @include        http://www.ritterburgwelt.de/rb/rbstart.php
 // @author         JonnyJD
-// @version        1.2.0.3
+// @version        1.2.1
 // ==/UserScript==
 
-var version = 'Kr\xE4henauge 1.2.0.3';
+var version = 'Kr\xE4henauge 1.2.1';
 var game = {
     standard: {
         links: new Array("rbstart",
