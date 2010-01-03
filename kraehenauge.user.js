05f5399602bb4256660ffc5cc3b37e792ac42efc
Johannes Dewender <jonerian@JonnyJD.net>
2010-01-03 17:23:36 +0000

    auge: markiere Trunkversionen mit [trunk]
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@468 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -44,7 +44,7 @@ if (document.title.indexOf("RB \xA9 - ") == 0
 if (document.title.indexOf("RB \xA9 - ") == 0) {
 
 var clientName = 'Kr\xE4henauge';
-var clientVersion = '1.4';
+var clientVersion = '1.4 [trunk]';
 var version = clientName + " " + clientVersion;
 var DEBUG = false;
 
