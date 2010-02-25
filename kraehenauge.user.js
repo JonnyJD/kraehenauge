d1a7ccd6049f124255f0d5edaae12015851eed74
Johannes Dewender <jonerian@JonnyJD.net>
2010-02-25 20:32:15 +0000

    auge: port fix [740] to branches, close #57
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@741 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -815,11 +815,11 @@ function visibleText(htmlPage)                                  // {{{2
     return copyText;
 }                                               // }}}2
 
-copyText = visibleText(wholePage);
 
 if (gamePage == "rbarmee") {
 
     if (wholePage.indexOf("fliehen") == -1) {
+        copyText = visibleText(wholePage);
         sendToHandler("send/text/armee", "dorftext", copyText, "DBAntwort");
     }
 }
@@ -830,6 +830,7 @@ if (gamePage == "rbfturm1"
     || gamePage == "rbfturms"
     || gamePage == "rbfhandelb") {
 
+    copyText = visibleText(wholePage);
     sendToHandler("send/text/turm", "text", copyText, "DBAntwort");
 }
 
