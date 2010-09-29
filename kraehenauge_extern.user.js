9d7315f53540368853e0dc8e980df7b9677f4a51
Johannes Dewender <jonerian@JonnyJD.net>
2010-09-29 14:50:38 +0000

    auge: tag tw-0.9.3, update ext-0.9.3 (unbenutzt)
    git-svn-id: http://svn.kraehen.org/tools/auge/branch/extern@838 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge_extern.user.js
+++ kraehenauge_extern.user.js
@@ -44,7 +44,7 @@ if (document.title.indexOf("RB \xA9 - ") == 0
 if (document.title.indexOf("RB \xA9 - ") == 0) {
 
 var clientName = 'Kr\xE4henauge: externe Edition';
-var clientVersion = '0.9.3 [trunk]';
+var clientVersion = '0.9.4 [trunk]';
 var version = clientName + " " + clientVersion;
 var DEBUG = false;
 
