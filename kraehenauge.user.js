1c8f3a715632c6b0e2ef33deb5724cdb0ef12c10
Johannes Dewender <jonerian@JonnyJD.net>
2011-11-26 09:52:34 +0000

    auge: -RK als Feind
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@934 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -297,7 +297,7 @@ var game = {
 // 32 = Raeuber
 // Trenner ist | (regExp ODER)
 var friendlyAllies = "(60|59|31|38)";
-var hostileAllies  = "(32|67)";
+var hostileAllies  = "(32)";
 
 // Einstellungen Ressourcenauswertung und Zugauswertung
 // Bei welcher anzahl verbleibender Tage welche Farbe benutzt wird:
