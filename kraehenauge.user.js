98f05943b2a155cb4b1f9d6de804a8270c9edb3d
Johannes Dewender <jonerian@JonnyJD.net>
2013-01-08 17:01:04 +0000

    auge: benutze localStorage fuer opera, re #117
    This also removes all this getRecoverableString cruft.
    We only use strings anyways.
    Previously cookies where used,
    but localStorage works in Opera 11 and we won't get lower than that.
    
    Additionally, this should work on other browsers, too;
    without GM emulation.
    
    
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@1004 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -84,191 +84,23 @@ if (document.title.indexOf("RB \xA9 - ") == 0
     }
 
     if (GM_api_incomplete()) {
-        /* folgende Fkt. entnommen aus
-         * http://www.howtocreate.co.uk/operaStuff/userjs/aagmfunctions.js
-         * version 1.3.1
-         */
-        GM_setValue = function( cookieName, cookieValue, lifeTime ) {
-            if( !cookieName ) {
-                return;
-            }
-            if( lifeTime == "delete" ) {
-                lifeTime = -10;
+        /* use localStorage */
+        GM_setValue = function(key, value) {
+            localStorage.setItem(key, value);
+        }
+
+        GM_getValue = function(key, defaultValue) {
+            value = localStorage.getItem(key);
+            if (value !== null) {
+                return value;
             } else {
-                lifeTime = 31536000;
+                return defaultValue
             }
-            document.cookie = escape( cookieName )
-                + "=" + escape( getRecoverableString( cookieValue ) )
-                + ";expires=" + ( new Date( ( new Date() ).getTime()
-                            + ( 1000 * lifeTime ) ) ).toGMTString()
-                + ";path=/";
         }
 
-        GM_getValue = function( cookieName, oDefault ) {
-            var cookieJar = document.cookie.split( "; " );
-            for( var x = 0; x < cookieJar.length; x++ ) {
-                var oneCookie = cookieJar[x].split( "=" );
-                if( oneCookie[0] == escape( cookieName ) ) {
-                    try {
-                        eval('var footm = '+unescape( oneCookie[1] ));
-                    } catch(e) {
-                        return oDefault;
-                    }
-                    return footm;
-                }
-            }
-            return oDefault;
-
+        GM_deleteValue = function(key) {
+            localStorage.removeItem(key);
         }
-
-        // nur der Vollstaendigkeit halber, wird nicht benoetigt
-        GM_deleteValue = function( oKey ) {
-            GM_setValue( oKey, '', 'delete' );
-        }
-
-        // wird von GM_setValue benoetigt
-        getRecoverableString = function(oVar,notFirst) {
-            var oType = typeof(oVar);
-            if( ( oType == 'null' ) || ( oType == 'object' && !oVar ) ) {
-                //most browsers say that the typeof for null is 'object',
-                //but unlike a real object,
-                //it will not have any overall value
-                return 'null';
-            }
-            if( oType == 'undefined' ) { return 'window.uDfXZ0_d'; }
-            if( oType == 'object' ) {
-                //Safari throws errors when comparing
-                //non-objects with window/document/etc
-                if( oVar == window ) { return 'window'; }
-                if( oVar == document ) { return 'document'; }
-                if( oVar == document.body ) { return 'document.body'; }
-                if( oVar == document.documentElement )
-                    { return 'document.documentElement'; }
-            }
-            if( oVar.nodeType && ( oVar.childNodes || oVar.ownerElement ) )
-                { return '{error:\'DOM node\'}'; }
-            if( !notFirst ) {
-                Object.prototype.toRecoverableString = function (oBn) {
-                    if( this.tempLockIgnoreMe )
-                        { return '{\'LoopBack\'}'; }
-                    this.tempLockIgnoreMe = true;
-                    var retVal = '{', sepChar = '', j;
-                    for( var i in this ) {
-                        if( i == 'toRecoverableString'
-                                || i == 'tempLockIgnoreMe'
-                                || i == 'prototype'
-                                || i == 'constructor' )
-                            { continue; }
-                        if( oBn && ( i == 'index' || i == 'input'
-                                    || i == 'length'
-                                    || i == 'toRecoverableObString' ) )
-                            { continue; }
-                        j = this[i];
-                        if( !i.match(basicObPropNameValStr) ) {
-                            //for some reason, you cannot use unescape
-                            //when defining peoperty names inline
-                            for( var x = 0; x < cleanStrFromAr.length; x++ ) {
-                                i =i.replace(cleanStrFromAr[x],cleanStrToAr[x]);
-                            }
-                            i = '\''+i+'\'';
-                        } else if( window.ActiveXObject
-                                && navigator.userAgent.indexOf('Mac') + 1
-                                && !navigator.__ice_version
-                                && window.ScriptEngine
-                                && ScriptEngine() == 'JScript'
-                                && i.match(/^\d+$/) ) {
-                            //IE mac does not allow numerical property names
-                            //to be used unless they are quoted
-                            i = '\''+i+'\'';
-                        }
-                        retVal += sepChar+i+':'+getRecoverableString(j,true);
-                        sepChar = ',';
-                    }
-                    retVal += '}';
-                    this.tempLockIgnoreMe = false;
-                    return retVal;
-                };
-                Array.prototype.toRecoverableObString =
-                    Object.prototype.toRecoverableString;
-                Array.prototype.toRecoverableString = function () {
-                    if( this.tempLock ) { return '[\'LoopBack\']'; }
-                    if( !this.length ) {
-                        var oCountProp = 0;
-                        for( var i in this ) {
-                            if( i != 'toRecoverableString'
-                                    && i != 'toRecoverableObString'
-                                    && i != 'tempLockIgnoreMe'
-                                    && i != 'prototype'
-                                    && i != 'constructor'
-                                    && i != 'index'
-                                    && i != 'input'
-                                    && i != 'length' )
-                            { oCountProp++; }
-                        }
-                        if( oCountProp )
-                            { return this.toRecoverableObString(true); }
-                    }
-                    this.tempLock = true;
-                    var retVal = '[';
-                    for( var i = 0; i < this.length; i++ ) {
-                        retVal += (i?',':'')+getRecoverableString(this[i],true);
-                    }
-                    retVal += ']';
-                    delete this.tempLock;
-                    return retVal;
-                };
-                Boolean.prototype.toRecoverableString = function () {
-                    return ''+this+'';
-                };
-                Date.prototype.toRecoverableString = function () {
-                    return 'new Date('+this.getTime()+')';
-                };
-                Function.prototype.toRecoverableString = function () {
-                    return this.toString().replace(/^\s+|\s+$/g,'').replace(/^function\s*\w*\([^\)]*\)\s*\{\s*\[native\s+code\]\s*\}$/i,'function () {[\'native code\'];}');
-                };
-                Number.prototype.toRecoverableString = function () {
-                        if( isNaN(this) ) { return 'Number.NaN'; }
-                        if( this == Number.POSITIVE_INFINITY )
-                            { return 'Number.POSITIVE_INFINITY'; }
-                        if( this == Number.NEGATIVE_INFINITY )
-                            { return 'Number.NEGATIVE_INFINITY'; }
-                        return ''+this+'';
-                };
-                RegExp.prototype.toRecoverableString = function () {
-                        return '\/'+this.source+'\/'+(this.global?'g':'')+(this.ignoreCase?'i':'');
-                };
-                String.prototype.toRecoverableString = function () {
-                        var oTmp = escape(this);
-                        if( oTmp == this ) { return '\''+this+'\''; }
-                        return 'unescape(\''+oTmp+'\')';
-                };
-            }
-            if( !oVar.toRecoverableString )
-                { return '{error:\'internal object\'}'; }
-            var oTmp = oVar.toRecoverableString();
-            if( !notFirst ) {
-                    //prevent it from changing for...in loops
-                    //that the page may be using
-                    delete Object.prototype.toRecoverableString;
-                    delete Array.prototype.toRecoverableObString;
-                    delete Array.prototype.toRecoverableString;
-                    delete Boolean.prototype.toRecoverableString;
-                    delete Date.prototype.toRecoverableString;
-                    delete Function.prototype.toRecoverableString;
-                    delete Number.prototype.toRecoverableString;
-                    delete RegExp.prototype.toRecoverableString;
-                    delete String.prototype.toRecoverableString;
-            }
-            return oTmp;
-        }
-
-        var basicObPropNameValStr = /^\w+$/
-        var cleanStrFromAr = new Array(/\\/g,/'/g,/"/g,/\r/g,/\n/g,/\f/g,/\t/g,
-                                new RegExp('-'+'->','g'),
-                                new RegExp('<!-'+'-','g'),/\//g)
-        var cleanStrToAr = new Array('\\\\','\\\'','\\\"','\\r','\\n','\\f',
-                            '\\t','-\'+\'->','<!-\'+\'-','\\\/');
-
     }
     //                                          }}}2
 
