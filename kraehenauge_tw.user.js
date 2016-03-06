de2c5082bb4bf0b55778d4207303b4f9352a0817
Johannes Dewender <jonerian@JonnyJD.net>
2016-03-06 11:34:34 +0000

    auge: merge [1084] to branches
    git-svn-id: http://svn.kraehen.org/tools/auge/branch/tw@1085 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge_tw.user.js
+++ kraehenauge_tw.user.js
@@ -1448,7 +1448,7 @@ if( gamePage == "rbarmee"
             if (gamePage == "rbarmee") {
                 // Position aus der Landschaftsaktualisierung
                 var pos = currentPos; // von aktueller Armee
-                var outerTD = divEntries[i].parentNode.parentNode;
+                var outerTD = divEntries[i].parentNode;
                 var name = outerTD.previousSibling.firstChild.firstChild.data;
                 if(name != "Bei:") {
                     /* "Bei:" -> Kapitaen des Schiffs auf dem man ist
