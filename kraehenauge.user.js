b641dd25d3a605b72c710fcdbcc75f3f8228f6ab
Johannes Dewender <jonerian@JonnyJD.net>
2016-03-25 10:11:24 +0000

    auge: also detect Aussenposten
    git-svn-id: http://svn.kraehen.org/tools/auge/trunk@1120 e0e46c49-be69-4f5a-ad62-21024a331aea

--- kraehenauge.user.js
+++ kraehenauge.user.js
@@ -1110,6 +1110,18 @@ if (gamePage == "rbarmee") {
                             addAlly(dorfElem, value.getAttribute("data-dorfallianz"));
                         }
                     }
+                    if (value.getAttribute("data-aptage")
+                            && x == xpos && y == ypos) {
+                        var level = value.getAttribute("data-aptage");
+                        var name = "Aussenposten";
+                        var besitzer = value.getAttribute("data-apbesitzer");
+                        var dorfElem = addDorf(feldElem, level, name,
+                                               besitzer);
+                        dorfElem.setAttribute("type", "ap");
+                        if (value.getAttribute("data-apallianz")) {
+                            addAlly(dorfElem, value.getAttribute("data-apallianz"));
+                        }
+                    }
                 }
             });
 
@@ -1157,6 +1169,16 @@ if (gamePage == "rbfturm1"
                                        besitzer, dorfdetail);
                 if (value.getAttribute("data-dorfallianz")) {
                     addAlly(dorfElem, value.getAttribute("data-dorfallianz"));
+                }
+            }
+            if (value.getAttribute("data-aptage")) {
+                var level = value.getAttribute("data-aptage");
+                var name = "Aussenposten";
+                var besitzer = value.getAttribute("data-apbesitzer");
+                var dorfElem = addDorf(feldElem, level, name, besitzer);
+                dorfElem.setAttribute("type", "ap");
+                if (value.getAttribute("data-apallianz")) {
+                    addAlly(dorfElem, value.getAttribute("data-apallianz"));
                 }
             }
         }
